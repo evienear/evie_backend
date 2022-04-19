891fa204957021dbc7fbd550ea38f4de5585492e
JuEnPeHa <jeph@nearlearning.app>
2022-04-18 23:46:44 -0500

    documentation started - 1

--- document evie backend.rs
+++ document evie backend.rs
@@ -18,18 +18,24 @@ update_price(nft_contract_id: AccountId, token_id: String, price: U128)
 //Receive the contract of the NFT and the token id and make the buy calling to process_purchase
 offer(nft_contract_id: AccountId, token_id: String)
 
+//Añade un deposito para storage a la cuenta que se le pase o al caller si no se le pasa una
+//Attach a deposit for storage to a passed account ID, and if no Account ID it takes the caller
+storage_deposit(account_id: Option<AccountId>) //El AccountID es opcional, si no se manda toma el caller - optional, if not passed take the caller
+
+//Permitimos a los usuarios retirar el storage depositado en exceso
+//Allow users to withdraw the storage deposit excess
+storage_withdraw() //Sin parametros extra, solo el que llama - No extra parameter, takes the caller
+
+//Retorna el balance minimo requerido para un NFT listado
+//Returns the minimum balance required for an NFT listed
+storage_minimum_balance()
 
 
 
 
 
-
-
-
-
-
-
-
+//Listando NFT
+//Listing NFT
 
 //Al momento de listar un NFT deben llamarse 2 funciones, la primera es storage_deposit Y SE LE ANEXA UN DEPOSITO
 //At the moment of listing an NFT we should call 2 functions, the first is storage_deposit A WE ATTACH A DEPOSIT
@@ -43,4 +49,5 @@ storage_deposit(account_id: Option<AccountId>) //El AccountID es opcional, si no
 //In order to list an NFT there should be called the funcion nft_approve in the orginal nft contract
 nft_approve(token_id: TokenId/*String*/, account_id: AccountId, msg: Option<String> /*Required*/)
 
+//Nada
 //internal_remove_sale(nft_contract_id: AccountId, token_id: String)
